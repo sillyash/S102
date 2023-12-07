49b111f577934cffc0c052e018c06f3fd57e6e70
Ash <59657132+sillyash@users.noreply.github.com>
2023-12-07 10:28:44 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "image.h"
+
+using namespace std;
+
+int main() {
+	
+	// Image par défaut dans l'énoncé (cf. image.h > constructeur pour les valeurs)
+	Image test;
+	
+	/// ------ TESTS MANUELS ------
+	// Méthode getPixel()
+	cout << "Get pixel (3,3): ";
+	cout << test.getPixel(3,3)[0] << ", " << test.getPixel(3,3)[1] << ", "  << test.getPixel(3,3)[2];
+	cout << endl << endl;
+	
+	// Méthode display()
+	cout << "Display:" << endl;
+	test.display();
+	
+	// Méthode composanteRouge()
+	cout << "Composante rouge:" << endl;
+	test.composanteRouge().display();
+
+	// Méthode detection()
+	cout << "Detection (255, 255, 0): ";
+	cout << test.detection(255, 255, 0) << endl << endl;
+
+	// Méthode niveauxGris()
+	cout << "Niveaux de gris:" << endl;
+	test.niveauxGris().display();
+	
+	/// --- TESTS AUTOMATIQUES ---
+
+	/// ------ FIN DES TESTS ------
+	
+	return 0;
+}
